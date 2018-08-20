49de4211caf864298d0034e31bd09d77bda1ba30
Niklas Carlsson <niklas.carlsson1994@gmail.com>
2018-08-20 20:59:10 +0200

    Add rel_bin for Windows
    * Ship with prebuilts to run on Windows

--- clean.sh
+++ clean.sh
@@ -8,9 +8,12 @@ rm -rf sfml-tmxloader
 rm -rf TGUI
 
 # clean unix cmake
-rm -rf bin/CMakeFiles/
-rm -rf build/
+rm -rf bin/CMakeFiles
+rm -rf build
 
 # clean cross platform binaries
 rm -f bin/Kobla-2D-Client*
 rm -f bin/*.dll.a
+
+# clean rel_bin
+rm -rf rel_bin/windows
--- create.sh
+++ create.sh
@@ -19,6 +19,33 @@ if [ $# -eq 0 ]; then
 else
 	if [ $1 = "clean" ]; then
 		build clean
+	elif [ $1 = "rel_bin" ]; then
+		./clean.sh
+		
+		# create Windows client
+		mkdir build; cd build
+		cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain-windows-mingw32.cmake ..
+		make -j $cores
+		
+		if [ $? -ne 0 ]; then
+			exit 1
+		fi
+		
+		cd ..
+		mkdir -p rel_bin/windows
+		
+		# copy data, executable, config and prebuilts to release
+		cp -r bin/data rel_bin/windows
+		cp bin/Kobla-2D-Client-Rebased.exe rel_bin/windows
+		cp bin/config rel_bin/windows
+		cp prebuilt/* rel_bin/windows
+		
+		cd rel_bin/windows
+		zip -r ../kobla_2d_client_windows.zip *
+		cd ../..
+		rm -rf rel_bin/windows
+		
+		./clean.sh
 	elif [ $1 = "run" ]; then
 		build
 		
